943576d56ca2ec6a1c9ef9e6e1d33a7e08c5849e
DeOxy <81672195+Ratchanon-Promsombut@users.noreply.github.com>
2024-06-06 20:51:25 +0700

    [ADD] EditProfilePage.dart

---
+++ lib/pages/edit_profile_page.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nampa_hub/src/widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Edit Profile Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.interTextTheme(),
+      ),
+      home: const MyEditProfilePage(),
+    );
+  }
+}
+
+class MyEditProfilePage extends StatefulWidget {
+  const MyEditProfilePage({super.key});
+
+  @override
+  _MyEditProfilePageState createState() => _MyEditProfilePageState();
+}
+
+class _MyEditProfilePageState extends State<MyEditProfilePage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _universityController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _universityController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Logo(),
+        ),
+        backgroundColor: const Color(0xFF1B8900),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30),
+                    child: ClipOval(
+                      child: Image(
+                        image: AssetImage('images/Beach.jpg'),
+                        width: 180,
+                        height: 180,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 30,
+                  right: 100,
+                  child: IconButton(
+                    icon: Icon(Icons.edit, color: Colors.black),
+                    onPressed: () {
+                      // Add functionality to change the image
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 30, left: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Current information',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xFF1B8900),
+                  ),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 10, bottom: 5),
+              child: Text(
+                'Username',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'King Mongkut’s University of Technology Thonburi (KMUTT)',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 30, left: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'New information',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xFF1B8900),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: TextFormField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'New Username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: TextFormField(
+                controller: _universityController,
+                decoration: const InputDecoration(
+                  labelText: 'New Education',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Container(
+                width: 380,
+                height: 50,
+                margin: const EdgeInsets.all(30.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle submit action here
+                    String newUsername = _usernameController.text;
+                    String newUniversity = _universityController.text;
+                    // You can use newUsername and newUniversity as needed
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
