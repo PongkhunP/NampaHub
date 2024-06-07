2f4b0c741e028f5bf05aaf61aca5ba2d6ddda4b1
DeOxy <81672195+Ratchanon-Promsombut@users.noreply.github.com>
2024-06-07 16:30:03 +0700

    [EDIT] Widget.dart Add historycard

--- lib/src/widget.dart
+++ lib/src/widget.dart
@@ -83,3 +83,90 @@ class ActivityCard extends StatelessWidget {
     );
   }
 }
+
+class ActivityHistoryCard extends StatelessWidget {
+  final String activityName;
+  final String location;
+  final String participation;
+  final String status;
+
+  const ActivityHistoryCard({
+    super.key,
+    required this.activityName,
+    required this.location,
+    required this.participation,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 6,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      height: 150, // Adjust the height as needed
+      width: double.infinity,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 130,
+              width: 130,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: const DecorationImage(
+                  image: AssetImage('images/Beach.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    activityName,
+                    style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                  Text(
+                    location,
+                    style: const TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                  const SizedBox(height: 5),
+                  const Padding(
+                    padding: EdgeInsets.only(top:30),
+                    child: Text(
+                      'Participation',
+                      style: TextStyle(fontSize: 15, color: Colors.black54, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Text(
+                    participation,
+                    style: const TextStyle(fontSize: 12, color: Colors.black54),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
