073aff43395e11a7d59e8746717ec25e2fbde469
IdSausage <pongkun.p@gmail.com>
2024-06-08 00:27:03 +0700

    [EDIT] app.js , index.js
    - add body-parser and router config to app.js
    - move database connection function from index.js to db.js
    - remove unnecessary function in index.js

--- server/index.js
+++ server/index.js
@@ -1,38 +1,11 @@
 require('dotenv').config();
-const mariadb = require('mariadb');
 
 const app = require('./app');
 const port = process.env.PORT;
 
-const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT
-})
-
-app.get('/data', async (req, res) => {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        const rows = await conn.query('SELECT * FROM user_account');
-        res.json(rows);
-    } catch (err) {
-        console.error('Error acquiring connection:', err);
-        res.status(500).send(err.toString());
-    } finally {
-        if (conn) {
-            conn.release(); // Use release instead of end for connection pools
-        }
-    }
-});
-
-
 app.get('/' , (req , res)=> {
     res.send('Hello world!!!');
 });
-
 
 app.listen(port , () => {
     console.log(`Server listening on Port http://${process.env.DB_HOST}:${port}`);
