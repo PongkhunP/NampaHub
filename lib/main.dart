7c7523e06ce6559c955b0e20da90328f42779d1f
IdSausage <pongkun.p@gmail.com>
2024-06-08 02:20:02 +0700

    [EDIT] main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nampa_hub/pages/home_page.dart';
+import 'package:nampa_hub/auth/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
-      home: HomeScreen(),
+      home: LoginPage(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MyHomePage();
-  }
-}
