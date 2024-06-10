df73e8058e59affd5af833d6bcc3b9a3755d2132
IdSausage <pongkun.p@gmail.com>
2024-06-10 23:50:10 +0700

    [EDIT] user.model.js
    - add few more endpoints function for registering user

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,17 +1,57 @@
 const pool = require('../configuration/db');
 
 class UserModel {
-  static async createUser(email, password) {
-    let conn;
+  static async createUserAccount(email, password, conn) {
     try {
-      conn = await pool.getConnection();
       const query = 'INSERT INTO user_account (email, password) VALUES (?, ?)';
       const result = await conn.query(query, [email, password]);
       return result;
     } catch (err) {
       throw err;
-    } finally {
-      if (conn) conn.release();
+    }
+  }
+
+  static async createUserPersonal(firstname , middlename , lastname , age , phone, user_id, conn )
+  {
+    try {
+      const query = 'INSERT INTO user_personal_data (first_name, middle_name, last_name, age, phone, user_id) VALUES (?, ?, ?, ?, ?, ?)';
+      const result = await conn.query(query , [firstname , middlename , lastname, age ,phone, user_id]);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createUserLocation(country , city, user_id, conn)
+  {
+    try {
+      const query = 'INSERT INTO user_location (country, city, user_id) VALUES (?, ?, ?)';
+      const result = await conn.query(query , [country , city, user_id]);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createUserWork(company_name , job, user_id, conn)
+  {
+    try {
+      const query = 'INSERT INTO user_work_data (company_name, jobs, user_id) VALUES (?, ?, ?)';
+      const result = await conn.query(query , [company_name , job, user_id]);
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async createUserEdu(institution_name , start_year , end_year, user_id, conn)
+  {
+    try {
+      const query = 'INSERT INTO user_education_data (edu_name, start_year, end_year, user_id) VALUES (?, ?, ?, ?)';
+      const result = await conn.query(query , [institution_name , start_year, end_year, user_id]);
+      return result;
+    } catch (error) {
+      throw error;
     }
   }
 
