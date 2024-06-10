263bb9096a47ff5066880f3ab21e40fc81a8de1d
IdSausage <pongkun.p@gmail.com>
2024-06-10 23:37:03 +0700

    [EDIT] main.dart
    - add dotenv initialization in the main function
    - add token verification at the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:nampa_hub/auth/login.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:nampa_hub/auth/main_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await dotenv.load(fileName: '.env');
+  String? storedToken = prefs.getString('token');
+  runApp(MyApp(
+    token: storedToken ?? '',
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String token;
+  const MyApp({super.key, required this.token});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: LoginPage(),
+    return MaterialApp(
+      title: 'NampaHub',
+      home: MainAuth(token: token),
     );
   }
 }
-
