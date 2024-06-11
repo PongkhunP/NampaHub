3ff2ef2e6d552f4ec876e99dd4aeba00dfdfc320
IdSausage <pongkun.p@gmail.com>
2024-06-11 18:12:28 +0700

    [EDIT] create_activity_two_page.dart
    - add activity parameter for receive activity information from the previous page
    - add text form field controller for tracking the input
    - add alert dialog for user to input the reward details
    - add image picker function for user to upload the image

--- lib/pages/create_activity_two_page.dart
+++ lib/pages/create_activity_two_page.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:nampa_hub/src/activity.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:nampa_hub/src/widget.dart'; // Make sure this import is correct
 
 final _formkey = GlobalKey<FormState>();
 
 class CreateActivityTwoPage extends StatefulWidget {
-  const CreateActivityTwoPage({super.key});
+  final Activity activity;
+  const CreateActivityTwoPage({super.key, required this.activity});
 
   @override
   State<CreateActivityTwoPage> createState() => _CreateActivityOnePageState();
@@ -13,36 +18,126 @@ class CreateActivityTwoPage extends StatefulWidget {
 List<String> options = ['Donate', 'No donate'];
 
 class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
-  String currentOption = options[0];
-  final List<TextEditingController> _goalControllers = [];
-  final TextEditingController _goalController = TextEditingController();
+  String donationOption = options[0];
+  final List<ActivityReward> _rewards = [];
+
+  final TextEditingController _participantsController = TextEditingController();
+  final TextEditingController _meetingLocationController =
+      TextEditingController();
+  final TextEditingController _eventLocationController =
+      TextEditingController();
+  final TextEditingController _organizerController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
   }
 
-  void _addGoal(String goal) {
-    if (goal.isNotEmpty) {
-      setState(() {
-        var controller = TextEditingController(text: goal);
-        _goalControllers.add(controller);
-      });
-    }
-  }
+  void _addReward() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        final TextEditingController rewardNameController =
+            TextEditingController();
+        final TextEditingController rewardDescriptionController =
+            TextEditingController();
+        XFile? imagefile;
 
-  void _removeGoal(int index) {
-    setState(() {
-      _goalControllers.removeAt(index);
-    });
+        return StatefulBuilder(
+          builder: (context, setState) {
+            Future<void> _pickImage() async {
+              final ImagePicker picker = ImagePicker();
+              final XFile? pickedFile =
+                  await picker.pickImage(source: ImageSource.gallery);
+
+              if (pickedFile != null) {
+                setState(() {
+                  imagefile = pickedFile;
+                });
+              }
+            }
+
+            return AlertDialog(
+              title: const Text('Add Reward'),
+              content: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      controller: rewardNameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Reward Name',
+                        border:
+                            OutlineInputBorder(), // Removes the underline border
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: rewardDescriptionController,
+                      decoration: const InputDecoration(
+                          hintText: 'Reward Description',
+                          border:
+                              OutlineInputBorder() // Removes the underline border
+                          ),
+                    ),
+                    const SizedBox(height: 10),
+                    GestureDetector(
+                      onTap: _pickImage,
+                      child: Container(
+                        height: 150,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: imagefile != null
+                            ? Image.file(File(imagefile!.path),
+                                fit: BoxFit.cover)
+                            : const Center(child: Text('Tap to pick an image')),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (rewardNameController.text.isNotEmpty &&
+                        imagefile != null) {
+                      setState(() {
+                        _rewards.add(ActivityReward(
+                          name: rewardNameController.text,
+                          rewardImage: imagefile!.path,
+                          description: rewardDescriptionController.text,
+                        ));
+                      });
+                      Navigator.of(context).pop();
+                    } else {
+                      // Show error if necessary fields are not filled
+                    }
+                  },
+                  child: const Text('Add'),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
   void dispose() {
-    for (var controller in _goalControllers) {
-      controller.dispose();
-    }
-    _goalController.dispose();
+    _participantsController.dispose();
+    _meetingLocationController.dispose();
+    _eventLocationController.dispose();
+    _organizerController.dispose();
     super.dispose();
   }
 
@@ -80,7 +175,7 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   child: TextFormField(
                       decoration: const InputDecoration(
-                        hintText: 'Amount participants ex. 10 people',
+                        hintText: 'Participants amount ex. 10 people',
                         border: InputBorder.none,
                         filled: true,
                         fillColor: Color.fromARGB(255, 230, 229, 229),
@@ -197,41 +292,80 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: TextFormField(
-                    controller: _goalController,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Reward (Optional)',
-                      suffixIcon: Padding(
-                        padding: const EdgeInsets.only(right: 6),
-                        child: IconButton(
-                          onPressed: () {
-                            _addGoal(_goalController.text);
-                            _goalController.clear();
-                          },
-                          icon: const Icon(Icons.add_circle),
-                          color: const Color(0xFF1B8900),
-                        ),
+                  child: GestureDetector(
+                    onTap: _addReward,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15, vertical: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              spreadRadius: 0.05,
+                              blurRadius: 5,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Reward (Optional)',
+                            style: TextStyle(
+                                color: Color(0xFF1B8900),
+                                fontSize: 16,
+                                fontWeight: FontWeight.normal),
+                          ),
+                          Icon(
+                            Icons.add_circle,
+                            color: Color(0xFF1B8900),
+                          )
+                        ],
                       ),
-                      filled: true,
-                      fillColor: const Color.fromARGB(255, 230, 229, 229),
-                      enabledBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                          borderSide: BorderSide.none),
-                      focusedBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                          borderSide: BorderSide.none),
-                      errorBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                          borderSide: BorderSide.none),
-                      focusedErrorBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                          borderSide: BorderSide.none),
                     ),
                   ),
                 ),
+                // Padding(
+                //   padding:
+                //       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                //   child: TextFormField(
+                //     controller: _rewardController,
+                //     decoration: InputDecoration(
+                //       border: InputBorder.none,
+                //       hintText: 'Reward (Optional)',
+                //       suffixIcon: Padding(
+                //         padding: const EdgeInsets.only(right: 6),
+                //         child: IconButton(
+                //           onPressed: () {
+                //             _addReward(_rewardController.text);
+                //             _rewardController.clear();
+                //           },
+                //           icon: const Icon(Icons.add),
+                //           color: const Color(0xFF1B8900),
+                //         ),
+                //       ),
+                //       filled: true,
+                //       fillColor: const Color.fromARGB(255, 230, 229, 229),
+                //       enabledBorder: const OutlineInputBorder(
+                //           borderRadius: BorderRadius.all(Radius.circular(7)),
+                //           borderSide: BorderSide.none),
+                //       focusedBorder: const OutlineInputBorder(
+                //           borderRadius: BorderRadius.all(Radius.circular(7)),
+                //           borderSide: BorderSide.none),
+                //       errorBorder: const OutlineInputBorder(
+                //           borderRadius: BorderRadius.all(Radius.circular(7)),
+                //           borderSide: BorderSide.none),
+                //       focusedErrorBorder: const OutlineInputBorder(
+                //           borderRadius: BorderRadius.all(Radius.circular(7)),
+                //           borderSide: BorderSide.none),
+                //     ),
+                //   ),
+                // ),
                 Column(
-                  children: List.generate(_goalControllers.length, (index) {
+                  children: List.generate(_rewards.length, (index) {
                     return Padding(
                       padding: const EdgeInsets.all(8),
                       child: Center(
@@ -260,14 +394,13 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                                     padding: const EdgeInsets.all(8.0),
                                     child: TextFormField(
                                       maxLines: null,
-                                      controller: _goalControllers[index],
                                       decoration: InputDecoration(
                                         contentPadding: const EdgeInsets.only(
                                             left: 2, top: 10),
                                         border: InputBorder.none,
                                         hintText: 'Goal',
                                         suffixIcon: IconButton(
-                                          onPressed: () => _removeGoal(index),
+                                          onPressed: () => {},
                                           icon: const Icon(Icons.remove_circle),
                                           color: Colors.red,
                                         ),
@@ -302,7 +435,7 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                     GestureDetector(
                       onTap: () {
                         setState(() {
-                          currentOption = options[0];
+                          donationOption = options[0];
                         });
                       },
                       child: Row(
@@ -310,10 +443,10 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                         children: [
                           Radio(
                             value: options[0],
-                            groupValue: currentOption,
+                            groupValue: donationOption,
                             onChanged: (value) {
                               setState(() {
-                                currentOption = value.toString();
+                                donationOption = value.toString();
                               });
                             },
                           ),
@@ -325,7 +458,7 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                     GestureDetector(
                       onTap: () {
                         setState(() {
-                          currentOption = options[1];
+                          donationOption = options[1];
                         });
                       },
                       child: Row(
@@ -333,10 +466,10 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                         children: [
                           Radio(
                             value: options[1],
-                            groupValue: currentOption,
+                            groupValue: donationOption,
                             onChanged: (value) {
                               setState(() {
-                                currentOption = value.toString();
+                                donationOption = value.toString();
                               });
                             },
                           ),
@@ -349,7 +482,7 @@ class _CreateActivityOnePageState extends State<CreateActivityTwoPage> {
                     // Add more options here if needed
                   ],
                 ),
-                if (currentOption == options[0])
+                if (donationOption == options[0])
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 20, vertical: 10),
