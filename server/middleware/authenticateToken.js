bf05273c51d0f22392a2b96144be5ec08a22d9e5
IdSausage <pongkun.p@gmail.com>
2024-06-18 20:52:56 +0700

    [ADD] authenticateToken.js
    - authenticate middleware before every endpoints was commit

---
+++ server/middleware/authenticateToken.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token) return res.status(401).send('Access Token required');
+
+    jwt.verify(token, process.env.SECRETKEY, (err, user) => {
+        if(err) return res.status(403).send('Invalid token');
+        req.user = user;
+        next();
+    })
+};
+
+module.exports = authenticateToken;
