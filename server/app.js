4a227cd563e77c8235592d0984be8799707eb4bf
IdSausage <pongkun.p@gmail.com>
2024-06-18 20:51:30 +0700

    [EDIT] app.js
    - add new path for activity endpoints

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const body_parser = require('body-parser');
 const userRouter = require('./routers/user.routes');
+const activityRouter = require('./routers/activity.routes');
 
 const app = express();
 app.use(cors())
 app.use(body_parser.json());
 app.use('/', userRouter);
+app.use('/activity', activityRouter);
+
 module.exports = app;
