3c471777d0f727c23453569c0c5891856af938c9
IdSausage <pongkun.p@gmail.com>
2024-06-18 20:49:55 +0700

    [EDIT] home_page.dart
    - add future builder for reading list of activity from backend to show in this page
    - implement filter function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,46 +1,48 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:nampa_hub/pages/activity_details_page.dart';
 import 'package:nampa_hub/pages/create_activity_one_page.dart';
 import 'package:nampa_hub/pages/profile_page.dart';
+import 'package:nampa_hub/src/activity.dart';
 import 'package:nampa_hub/src/widget.dart';
+import '../mid/activity_services.dart';
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
-  final List<String> activityNames = [
-    'Beach Bitch',
-    'Mountain Hiking',
-    'Forest Trek',
-    'Wildlife Safari',
-    'River Rafting',
-    'Cave Exploration',
-  ];
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  final List<String> locations = [
-    'Phuket',
-    'Rocky Mountains',
-    'Pacific Northwest',
-    'Serengeti',
-    'Colorado River',
-    'Carlsbad Caverns',
-  ];
+class _MyHomePageState extends State<MyHomePage> {
+  String currentFilter = 'Marine';
+  late Future<List<ActivityListItem>> futureActivities;
+
+  @override
+  void initState() {
+    super.initState();
+    futureActivities = ActivityService.getActivities(currentFilter);
+  }
+
+  void setFilter(String option) {
+    setState(() {
+      currentFilter = option;
+      futureActivities = ActivityService.getActivities(currentFilter);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const Spacer(
-              flex: 3,
-            ),
-            const Center(
-              child: Logo(),
-            ),
-            const Spacer(
-              flex: 2,
-            ),
+            const Spacer(flex: 3),
+            const Center(child: Logo()),
+            const Spacer(flex: 2),
             GestureDetector(
               onTap: () {
                 Navigator.push(context, MaterialPageRoute(
@@ -61,7 +63,6 @@ class MyHomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             TextField(
-              // controller: _textController,
               decoration: InputDecoration(
                 hintText: "Search...",
                 prefixIcon: const Icon(Icons.search),
@@ -73,138 +74,222 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 15),
-            Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(1),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF1797BF),
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: const Text(
-                    'Water',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF1B8900),
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setFilter('Marine');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1797BF),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text(
+                        'Water',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    child: const Text(
-                      'Forest',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setFilter('Forest');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1B8900),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: const Text(
+                        'Forest',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 3),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setFilter('Other');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>(
+                          (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.pressed)) {
+                              return const Color(
+                                  0xFFB0B0B0); // Color when pressed
+                            }
+                            return const Color(0xFFE0E0E0); // Default color
+                          },
+                        ),
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.black),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(color: Colors.black, width: 2),
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        'Other',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             const SizedBox(height: 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  children: [
-                    Text(
-                      'Found ',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black, // Text color set to black
-                      ),
-                    ),
-                    Text(
-                      '5,000 Activities',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green, // Text color set to green
-                      ),
-                    ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) {
-                        return const CreateActivityOnePage();
-                      },
-                    ));
-                  },
-                  icon: const CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    radius: 15,
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
             Expanded(
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  double height = 230;
-                  double width = 180;
-                  int itemsPerRow = 2;
-
-                  if (constraints.maxWidth < 600) {
-                    height = 250;
-                    width = 300;
-                    itemsPerRow = 1;
-                  }
-
-                  return ListView.builder(
-                    itemCount: (activityNames.length / itemsPerRow).ceil(),
-                    itemBuilder: (BuildContext context, int index) {
-                      int startIndex = index * itemsPerRow;
-                      int endIndex = (index * itemsPerRow + itemsPerRow)
-                          .clamp(0, activityNames.length);
-
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 16),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: List.generate(
-                            endIndex - startIndex,
-                            (subIndex) => Padding(
-                              padding: const EdgeInsets.only(right: 8),
-                              child: GestureDetector(
-                                onTap: () {
-                                  Navigator.push(context, MaterialPageRoute(
-                                    builder: (context) {
-                                      return const ActivityDetailsPage();
-                                    },
-                                  ));
-                                },
-                                child: ActivityCard(
-                                  activityName:
-                                      activityNames[startIndex + subIndex],
-                                  location: locations[startIndex + subIndex],
-                                  cardHeight: height,
-                                  cardWidth: width,
+              child: FutureBuilder<List<ActivityListItem>>(
+                future: futureActivities,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No activities found'));
+                  } else {
+                    return Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                const Text(
+                                  'Found ',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        Colors.black, // Text color set to black
+                                  ),
+                                ),
+                                Text(
+                                  '${snapshot.data?.length} ${snapshot.data?.length == 1 ? 'Activity' : 'Activities'}',
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color:
+                                        Colors.green, // Text color set to green
+                                  ),
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) {
+                                    return const CreateActivityOnePage();
+                                  },
+                                ));
+                              },
+                              icon: const CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: 15,
+                                child: Icon(
+                                  Icons.add,
+                                  color: Colors.white,
                                 ),
                               ),
                             ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Expanded(
+                          child: LayoutBuilder(
+                            builder: (context, constraints) {
+                              double height = 230;
+                              double width = 180;
+                              int itemsPerRow = 2;
+
+                              if (constraints.maxWidth < 600) {
+                                height = 250;
+                                width = 300;
+                                itemsPerRow = 1;
+                              }
+
+                              return ListView.builder(
+                                itemCount: (snapshot.data!.length / itemsPerRow)
+                                    .ceil(),
+                                itemBuilder: (BuildContext context, int index) {
+                                  int startIndex = index * itemsPerRow;
+                                  int endIndex =
+                                      (index * itemsPerRow + itemsPerRow)
+                                          .clamp(0, snapshot.data!.length);
+
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 16),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: List.generate(
+                                        endIndex - startIndex,
+                                        (subIndex) {
+                                          ActivityListItem activity = snapshot
+                                              .data![startIndex + subIndex];
+                                          Uint8List imageBytes = base64Decode(
+                                              activity
+                                                  .activityMedia.base64Image);
+                                          return Padding(
+                                            padding:
+                                                const EdgeInsets.only(right: 8),
+                                            child: GestureDetector(
+                                              onTap: () {
+                                                Navigator.push(context,
+                                                    MaterialPageRoute(
+                                                  builder: (context) {
+                                                    return ActivityDetailsPage(
+                                                        activityId:
+                                                            activity.id);
+                                                  },
+                                                ));
+                                              },
+                                              child: ActivityCard(
+                                                activityName: activity.title,
+                                                location:
+                                                    activity.eventLocation,
+                                                imageBytes:
+                                                    imageBytes, // Pass the decoded image bytes
+                                                cardHeight: height,
+                                                cardWidth: width,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  );
+                                },
+                              );
+                            },
                           ),
                         ),
-                      );
-                    },
-                  );
+                      ],
+                    );
+                  }
                 },
               ),
             ),
